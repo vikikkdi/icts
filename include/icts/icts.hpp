2fed0d4df200a53b58ea7aafee1deb799ec067a2
Vikraman Sathiyanarayanan <vikramansathiyanarayanan@vikramans-mbp.home>
2020-03-26 18:09:41 -0700

    mdd construction done

--- include/icts/icts.hpp
+++ include/icts/icts.hpp
@@ -3,9 +3,13 @@
 
 #include <vector>
 #include <queue>
-#include <unordered_set> 
+#include <unordered_set>
+#include <unordered_map>
 
-typedef std::pair<int, std::pair<int, std::pair<int, int> > > p_pair;
+typedef std::pair<int, int> pair_1;
+typedef std::pair<int, pair_1 > pair_2;
+typedef std::pair<int, pair_2 > pair_3;
+typedef std::tuple<int, int, int> tup;
 
 class HighLevelNode{
 public:
@@ -17,6 +21,10 @@ public:
 			ans += cost[i];
 		}
 		return ans;
+	}
+
+	int get_cost(int i){
+		return cost[i];
 	}
 
 	int sum(){
@@ -66,12 +74,199 @@ namespace std {
 	};
 }
 
+template<typename Mapf>
+class MDD{
+public:
+	MDD(int x, int y):	x_dim(x), y_dim(y)	{}
+
+	void construct_MDD_first_time(Mapf mapf, int depth){
+		cur_depth = depth;
+		for(int c=0; c<depth; c++){
+			for(int i=0; i<x_dim; i++){
+				for(int j=0; j<y_dim; j++){
+					adjacency_list[std::make_tuple(i, j, c)] = std::vector<tup>();
+				}
+			}
+		}
+
+		for(int i=0; i<x_dim; i++){
+			for(int j=0; j<y_dim; j++){
+				std::vector<pair_1 > neighbors = mapf.get_graph().get_neighbor(i, j);
+				neighbors.emplace_back(std::make_pair(i, j));
+				for(int c=0; c<depth-1; c++){
+					//Create the adjacency list for tuple (i, j, c)
+					for(auto it=neighbors.begin(); it!=neighbors.end(); ++it){
+						adjacency_list[std::make_tuple(i, j, c)].push_back(std::make_tuple(it->first, it->second, c+1));
+						adjacency_list[std::make_tuple(it->first, it->second, c+1)].push_back(std::make_tuple(i, j, c));
+					}
+				}
+			}
+		}
+		//print_MDD();
+	}
+
+	void increment_depth(Mapf mapf){
+		int depth = cur_depth;
+		cur_depth++;
+		for(int i=0; i<x_dim; i++){
+			for(int j=0; j<y_dim; j++){
+				adjacency_list[std::make_tuple(i, j, depth)] = std::vector<tup>();
+			}
+		}
+		for(int i=0; i<x_dim; i++){
+			for(int j=0; j<y_dim; j++){
+				std::vector<pair_1 > neighbors = mapf.get_graph().get_neighbor(i, j);
+				neighbors.emplace_back(std::make_pair(i, j));
+					//Create the adjacency list for tuple (i, j, c)
+				for(auto it=neighbors.begin(); it!=neighbors.end(); ++it){
+					adjacency_list[std::make_tuple(i, j, depth-1)].push_back(std::make_tuple(it->first, it->second, depth));
+					adjacency_list[std::make_tuple(it->first, it->second, depth)].push_back(std::make_tuple(i, j, depth-1));
+				}
+			}
+		}
+	}
+
+	int get_cur_depth(){	return cur_depth;	}
+
+	std::map<tup, std::vector<tup> > get_DAG(pair_1 start, pair_1 goal, int depth){
+		std::map<tup, std::vector<tup> > DAG_formed;
+		std::map<tup, std::vector<tup> > bfs_start;
+		std::map<tup, std::vector<tup> > bfs_goal;
+
+		std::map<tup, int> visited;
+
+		std::queue<tup> bfs_queue;
+
+		bfs_queue.push(std::make_tuple(start.first, start.second, 0));
+		visited[std::make_tuple(start.first, start.second, 0)] = 1;
+
+		while(!bfs_queue.empty()){
+			tup n = bfs_queue.front();
+			bfs_queue.pop();
+
+			if(bfs_start.find(n) == bfs_start.end()){
+				bfs_start[n] = std::vector<tup>();
+			}
+
+			if(std::get<2>(n) == depth){
+				continue;
+			}
+
+			for(auto it=adjacency_list[n].begin(); it!=adjacency_list[n].end(); ++it){
+				int x = std::get<0>(*it);
+				int y = std::get<1>(*it);
+				int c = std::get<2>(*it);
+
+				if(c == std::get<2>(n)+1){
+					if(visited.find(std::make_tuple(x, y, c)) == visited.end())
+						bfs_queue.push(*it);
+					visited[std::make_tuple(x, y, c)] = 1;
+					bfs_start[n].push_back(*it);
+				}
+			}
+		}
+		
+		visited.clear();
+		bfs_queue = std::queue<tup>();
+
+		bfs_queue.push(std::make_tuple(goal.first, goal.second, depth));
+		visited[std::make_tuple(goal.first, goal.second, depth)] = 1;
+
+		while(!bfs_queue.empty()){
+			tup n = bfs_queue.front();
+			bfs_queue.pop();
+
+			if(bfs_goal.find(n) == bfs_goal.end()){
+				bfs_goal[n] = std::vector<tup>();
+			}
+
+
+			if(std::get<2>(n) == 0){
+				continue;
+			}
+
+			for(auto it=adjacency_list[n].begin(); it!=adjacency_list[n].end(); ++it){
+				int x = std::get<0>(*it);
+				int y = std::get<1>(*it);
+				int c = std::get<2>(*it);
+				
+				if(c+1 == std::get<2>(n)){
+					if(visited.find(std::make_tuple(x, y, c)) == visited.end())
+						bfs_queue.push(*it);
+					visited[std::make_tuple(x, y, c)] = 1;
+					bfs_goal[n].push_back(*it);
+				}
+			}
+		}
+
+		for(auto it1 = bfs_start.begin(); it1 != bfs_start.end(); ++it1){
+			auto it2 = bfs_goal.find(it1->first);
+			if(it2 != bfs_goal.end()){
+				DAG_formed[it1->first] = std::vector<tup>();
+				if(std::get<0>(it1->first) == goal.first && std::get<1>(it1->first) == goal.second && std::get<2>(it1->first) == depth){
+					continue;
+				}
+				else if(it1->second.size()>0 && it2->second.size()>0){
+					for(auto m1=it1->second.begin(); m1!=it1->second.end(); ++m1){					
+						int x1 = std::get<0>(*m1);
+						int y1 = std::get<1>(*m1);
+						int c1 = std::get<2>(*m1);
+						for(auto m2=it2->second.begin(); m2!=it2->second.end(); ++m2){					
+							int x2 = std::get<0>(*m2);
+							int y2 = std::get<1>(*m2);
+							int c2 = std::get<2>(*m2);
+							if(x1==x2 && y1==y2){
+								DAG_formed[it1->first].push_back(*m1);
+							}
+						}
+					}
+				}
+				else if(it1->second.size() == 0){
+					DAG_formed[it1->first] = it2->second;
+				}
+				else if(it2->second.size() == 0){
+					DAG_formed[it1->first] = it1->second;
+				}
+			}
+		}
+		//print_map(DAG_formed);
+
+		return DAG_formed;
+	}
+
+	void print_MDD(){
+		for(auto it1 = adjacency_list.begin(); it1!=adjacency_list.end(); ++it1){
+			std::cout<<std::get<0>(it1->first)<<" "<<std::get<1>(it1->first)<<" "<<std::get<2>(it1->first)<<" :: ";
+			for(auto it2 = it1->second.begin(); it2!=it1->second.end(); ++it2){
+				std::cout<<"("<<std::get<0>(*it2)<<","<<std::get<1>(*it2)<<","<<std::get<2>(*it2)<<") ";
+			}
+			std::cout<<std::endl;
+		}
+	}
+
+	void print_map(std::map<tup, std::vector<tup> > mp){
+		for(auto it1 = mp.begin(); it1!=mp.end(); ++it1){
+			std::cout<<std::get<0>(it1->first)<<" "<<std::get<1>(it1->first)<<" "<<std::get<2>(it1->first)<<" :: ";
+			for(auto it2 = it1->second.begin(); it2!=it1->second.end(); ++it2){
+				std::cout<<"("<<std::get<0>(*it2)<<","<<std::get<1>(*it2)<<","<<std::get<2>(*it2)<<") ";
+			}
+			std::cout<<std::endl;
+		}
+	}
+
+private:
+	std::map<tup, std::vector<tup> > adjacency_list;
+	int x_dim;
+	int y_dim;
+	int cur_depth;
+};
+
 namespace ICT{
 	template<typename Mapf>
 	class ICTS{
 	public:
 		ICTS()	{}
-		bool search(Mapf mapf, std::vector<std::pair<int, int> > starts, std::pair<int, std::vector<std::pair<int, int> > > *solution){
+		bool search(Mapf mapf, std::vector<pair_1 > starts, std::pair<int, std::vector<pair_1 > > *solution){
 			std::vector<int> path_lengths = find_shortest_path(mapf, starts);
 			int num_of_agents = starts.size();
 			HighLevelNode n(path_lengths);
@@ -79,6 +274,9 @@ namespace ICT{
 			std::queue<HighLevelNode> high_level_search;
 			std::unordered_set<HighLevelNode> visited;
 
+			MDD<Mapf> mdd(mapf.get_x(), mapf.get_y());
+			mdd.construct_MDD_first_time(mapf, *(std::max_element(path_lengths.begin(), path_lengths.end())));
+			
 			high_level_search.push(n);
 			visited.insert(n);
 
@@ -87,9 +285,14 @@ namespace ICT{
 				high_level_search.pop();
 
 				//Perform the low level search on the node, If it succeeds, then return the solution
+				if(low_level_search(mdd, node, mapf, starts)){
+					//Solution Found
+					std::cout<<node.sum()<<std::endl;
+					break;
+				}
 				
-				//Dummy test
-				if(node.sum() >1000){	
+				//Dummy test for exiting the search
+				if(node.sum() >237){	
 					std::cout<<"Do the Low Level Search "<<node.sum()<<std::endl;
 					break;
 				}
@@ -107,23 +310,39 @@ namespace ICT{
 			return true;
 		}
 
-		int heuristic(std::pair<int, int> a, std::pair<int, int> b){
+		bool low_level_search(MDD<Mapf> mdd, HighLevelNode nodeHL, Mapf mapf, std::vector<pair_1 > starts){
+			std::vector<pair_1 > goals = mapf.get_goals();
+			std::vector< std::map<tup, std::vector<tup> > > mdds;
+			//Build the MDD for each agent
+			for(int i=0; i<starts.size(); i++){
+				pair_1 start = starts[i];
+				pair_1 goal = goals[i];
+				if(mdd.get_cur_depth()<=nodeHL.get_cost(i)){
+					mdd.increment_depth(mapf);
+				}
+				mdds.push_back(mdd.get_DAG(start, goal, nodeHL.get_cost(i)));
+			}
+			std::cout<<mdds.size()<<std::endl;
+			return false;
+		}
+
+		int heuristic(pair_1 a, pair_1 b){
 			return std::abs(a.first-b.first) + std::abs(a.second-b.second);
 		}
 
-		std::vector<int> find_shortest_path(Mapf mapf, std::vector<std::pair<int, int> > starts){
-			std::vector<std::pair<int, int> > goals = mapf.get_goals();
+		std::vector<int> find_shortest_path(Mapf mapf, std::vector<pair_1 > starts){
+			std::vector<pair_1 > goals = mapf.get_goals();
 			std::vector<int> path_lengths(starts.size());
 
 			for(int i=0; i<starts.size(); i++){
 				std::vector< std::vector<int> > visited(mapf.get_x(), std::vector<int> (mapf.get_y(), -1));
-				std::pair<int, int> goal= goals[i];
+				pair_1 goal= goals[i];
 
-				std::priority_queue<p_pair, std::vector<p_pair>, std::greater<p_pair> > pq;
+				std::priority_queue<pair_3, std::vector<pair_3>, std::greater<pair_3> > pq;
 				pq.push(std::make_pair(heuristic(starts[i], goals[i]), std::make_pair(0, starts[i]))); 
 
 				while(!pq.empty()){
-					p_pair node = pq.top();
+					pair_3 node = pq.top();
 					pq.pop();
 
 					int g = node.second.first;
@@ -138,12 +357,12 @@ namespace ICT{
 					}
 
 					visited[node_x][node_y] = g;
-					std::vector<std::pair<int, int> > neighbors = mapf.get_graph().get_neighbor(node_x, node_y);
+					std::vector<pair_1 > neighbors = mapf.get_graph().get_neighbor(node_x, node_y);
 					for(auto it = neighbors.begin(); it != neighbors.end(); ++it){
 						int neigh_x = it->first;
 						int neigh_y = it->second;
 						if(visited[neigh_x][neigh_y] == -1){
-							std::pair<int, int> neigh = std::make_pair(neigh_x, neigh_y);
+							pair_1 neigh = std::make_pair(neigh_x, neigh_y);
 							int f = heuristic(neigh, goal) + g + 1;
 							pq.push(std::make_pair(f, std::make_pair(g+1, neigh)));
 						}
